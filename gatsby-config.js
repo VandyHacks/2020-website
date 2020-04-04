f6dab935cc49c5f3e5c51640992463fbc8d5c3e0
Chinchuluun1029 <chinchuluun1029@gmail.com>
2020-04-04 18:07:33 -0400

    edited gatsby-config for vh

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `VandyHacks Promo Website`,
+    description: `Official promotional website for VandyHacks VII`,
+    author: `VandyHacks`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
