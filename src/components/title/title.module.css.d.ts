86954ce6857993b89b9d7044c6458112cdd90535
Sam Lee <samuel.r.lee@vanderbilt.edu>
2020-06-14 17:22:29 -0400

    Simplified title, now has assets, also fixed color of about

--- src/components/title/title.module.css.d.ts
+++ src/components/title/title.module.css.d.ts
@@ -1,5 +1,2 @@
-export const banner: string;
-export const typing: string;
-export const maintitle: string;
-export const subtitle: string;
+export const titleItem: string;
 export const title: string;
--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -1,29 +1,36 @@
 import React from 'react';
+import Banner from '../../assets/retroedn-used.png';
+import Maintitle from '../../assets/vandygoldhacks.png'
+import Subtitle from '../../assets/thehackathon.png'
 import * as styles from './title.module.css'
 
 const Title: React.FC<{}> = () => (
-    <div className={styles.title}>
-        <Banner />
+    <div id={styles.title}>
+        <img src={Banner} className={styles.titleItem}></img>
+        <img src={Maintitle} className={styles.titleItem}></img>
+        <img src={Subtitle} className={styles.titleItem}></img>
+        {/* <Banner />
         <Maintitle />
-        <Subtitle />
+        <Subtitle /> */}
     </div>
 )
 
-const Banner: React.FC<{}> = () => (
-    <div className={styles.banner}>
-        <h2>{' '}retro edn.</h2>
-    </div>
-)
-const Maintitle: React.FC<{}> = () => (
-    <div className={styles.maintitle}>
-        <h1>VandyHacks</h1>
-    </div>
-)
+// const Banner: React.FC<{}> = () => (
+//     // <div className={styles.banner}>
+//     //     <h2>{' '}retro edn.</h2>
+//     // </div>
+//     <img src={RetroEdn}></img>
+// )
+// const Maintitle: React.FC<{}> = () => (
+//     <div className={styles.maintitle}>
+//         <h1>VandyHacks</h1>
+//     </div>
+// )
 
-const Subtitle: React.FC<{}> = () => (
-    <div className={styles.subtitle}>
-        <h2>The Hackathon</h2>
-    </div>
-)
+// const Subtitle: React.FC<{}> = () => (
+//     <div className={styles.subtitle}>
+//         <h2>The Hackathon</h2>
+//     </div>
+// )
 
 export default Title
