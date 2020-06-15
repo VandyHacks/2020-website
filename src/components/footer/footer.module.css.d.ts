9419d3fe989abce76a84d5f05a0b7afcf6724315
Sam Lee <samuel.r.lee@vanderbilt.edu>
2020-06-15 14:55:40 -0400

    footer styling and email

--- src/components/footer/footer.module.css.d.ts
+++ src/components/footer/footer.module.css.d.ts
@@ -1,3 +1,4 @@
+export const right: string;
 export const footerMain: string;
 export const retroLogo: string;
 export const socials: string;
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -7,55 +7,61 @@ import TwitterLogo from '../../assets/twitter icon.png';
 
 import * as styles from './footer.module.css'
 
-const Footer: React.FC<{}> = () => (
-  <div id={styles.footerMain}>
-    <footer>
-      <span className="right">
-        {/* <p>{'Made with <3 and < / > at VandyHacks'}</p> */}
-        <div className={styles.socials}>
-          {/* Facebook Icon */}
-          <a
-            id="facebook-container"
-            href="https://www.facebook.com/vandyhacks/"
-            target="_blank"
-            rel="noopener"
-          >
-            <img src={FacebookLogo} />
-          </a>
+const Footer: React.FC<{}> = () => {
+  <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
+  return (
+    <div id={styles.footerMain}>
+      <footer className='nes-container is-dark is-rounded'>
+        <span className={styles.right}>
+          {/* <p>{'Made with <3 and < / > at VandyHacks'}</p> */}
+          Contact us!
+          <br/>
+          email@vanderbilt.edu
+          <div className={styles.socials}>
+            {/* Facebook Icon */}
+            <a
+              id="facebook-container"
+              href="https://www.facebook.com/vandyhacks/"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src={FacebookLogo} />
+            </a>
 
-          {/* <!-- Instagram Icon --> */}
-          <a
-            id="instagram-container"
-            href="https://www.instagram.com/vandyhacks/"
-            target="_blank"
-            rel="noopener"
-          >
-            <img src={InstagramLogo} />
-          </a>
+            {/* <!-- Instagram Icon --> */}
+            <a
+              id="instagram-container"
+              href="https://www.instagram.com/vandyhacks/"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src={InstagramLogo} />
+            </a>
 
-          {/* <!-- Github Icon --> */}
-          <a
-            id="github-container"
-            href="https://github.com/VandyHacks"
-            target="_blank"
-          >
-            <img src={GithubLogo} />
-          </a>
+            {/* <!-- Github Icon --> */}
+            <a
+              id="github-container"
+              href="https://github.com/VandyHacks"
+              target="_blank"
+            >
+              <img src={GithubLogo} />
+            </a>
 
-          {/* <!-- Twitter Icon --> */}
-          <a
-            id="twitter-container"
-            href="https://twitter.com/vandyhacks"
-            target="_blank"
-            rel="noopener"
-          >
-            <img src={TwitterLogo} />
-          </a>
-        </div>
-      </span>
-    </footer>
-    <img id={styles.retroLogo} src={RetroLogo} alt="RetroLogo" />
-  </div>
-);
+            {/* <!-- Twitter Icon --> */}
+            <a
+              id="twitter-container"
+              href="https://twitter.com/vandyhacks"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src={TwitterLogo} />
+            </a>
+          </div>
+        </span>
+      </footer>
+      <img id={styles.retroLogo} src={RetroLogo} alt="RetroLogo" />
+    </div>
+  );
+}
 
 export default Footer;
