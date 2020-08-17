485827b372aa1ac9daa41ba3d2920f33142000bd
SamLee514 <samuel.r.lee@vanderbilt.edu>
2020-08-17 01:10:38 -0400

    switching bracnhes

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from 'react';
+import { useSpring, animated } from 'react-spring';
 import PropTypes from "prop-types";
-import map from '../../assets/map-wip2.png'
+import map from '../../assets/map.png'
 
 // Import squirrel images
 import br from '../../assets/squirrel/back-rest.png'
@@ -18,44 +19,54 @@ import * as styles from './game.module.css'
 import "./game.module.css";
 
 const Game = () => {
-  const h = 10;
-  const w = 20;
+  const h = 30;
+  const w = 60;
   const rectHeight = 860;
   const rectWidth = 1720;
   const [squirrelPose, setSquirrelPose] = useState(fr)
-  const [squirrelX, setSquirrelX] = useState(10)
-  const [squirrelY, setSquirrelY] = useState(7)
+  const [squirrelX, setSquirrelX] = useState(30)
+  const [squirrelY, setSquirrelY] = useState(21)
   const [targetX, setTargetX] = useState(squirrelX)
   const [targetY, setTargetY] = useState(squirrelY)
+  const [isMoving, setMoving] = useState(false);
   useEffect(() => {
-    // let interval: number;
-    // interval = 0;
-    // if (squirrelX != targetX && squirrelY != targetY) {
-    //   interval = setInterval(() => {
-    //     if (squirrelX != targetX) {
-    //       setSquirrelX(targetX > squirrelX ? squirrelX + 1 : squirrelX - 1);
-    //       console.log('')
-    //     } else if (squirrelX != targetY) {
-    //       setSquirrelY(targetY > squirrelY ? squirrelY + 1 : squirrelY - 1);
-    //     }
-    //   })
-    // } else {
-    //   clearInterval(interval);
-    // }
+
     const interval = setInterval(() => {
-      if (squirrelX != targetX) {
-        setSquirrelX(targetX > squirrelX ? squirrelX + 1 : squirrelX - 1);
-        console.log('')
-      } else if (squirrelY != targetY) {
-        setSquirrelY(targetY > squirrelY ? squirrelY + 1 : squirrelY - 1);
-      } else {
-        clearInterval(interval);
+      if (isMoving) {
+        console.log('isMoving:', isMoving)
+        console.log('Moving!')
+        if (targetY > squirrelY) {
+          console.log('Moving up')
+          setSquirrelPose(fw);
+          setSquirrelY(squirrelY + 1);
+        } else if (targetY < squirrelY) {
+          console.log('Moving down')
+          setSquirrelPose(bw);
+          setSquirrelY(squirrelY - 1);
+        }
+        if (targetX > squirrelX) {
+          console.log('Moving left')
+          setSquirrelPose(rw0);
+          setSquirrelX(squirrelX + 1);
+        } else if (targetX < squirrelX) {
+          console.log('Moving right')
+          setSquirrelPose(lw0);
+          setSquirrelX(squirrelX - 1);
+        }
+        if (squirrelX == targetX && squirrelY == targetY) {
+          console.log('clear interval');
+          clearInterval(interval);
+          setSquirrelPose(fr);
+          setMoving(false);
+        }
       }
-      console.log('target', targetX, targetY, 'squirrel', squirrelX, squirrelY);
-    }, 500);
+    }, 300);
     return () => clearInterval(interval);
-  }, [squirrelX, squirrelY, targetX, targetY])
+  }, [squirrelX, squirrelY, targetX, targetY, isMoving])
+
   const initiateMovement = e => {
+    setMoving(true);
+    console.log('isMoving:', isMoving)
     const rect = e.target.getBoundingClientRect()
     // Discretize x and y into grid cells
     setTargetX(Math.round(w * (e.clientX - rect.left) / rectWidth));
@@ -66,16 +77,27 @@ const Game = () => {
   }
 
   const squirrelStyle = {
-    gridColumn: squirrelX,
-    gridRow: squirrelY,
+    gridColumn: `${squirrelX - 1} / ${squirrelX + 2}`,
+    gridRow: `${squirrelY - 1} / ${squirrelY + 2}`,
   }
+
+  const bounce = useSpring({
+    from: {transform: 'translate(0px, 0px)'},
+    to: async (next, cancel) => {
+      while (!isMoving) {
+        console.log('bouncing')
+        await next({transform: 'translate(0px, -10px)'})
+        await next({transform: 'translate(0px, 10px)'})
+      }
+    },
+  })
 
   return (
     <div id={styles.gameBoard} onClick={initiateMovement}>
       <img id={styles.map} src={map}></img>
-      <img id={styles.squirrel}
+      <animated.img id={styles.squirrel}
            src={squirrelPose}
-           style={squirrelStyle}></img>
+           style={squirrelStyle}></animated.img>
     </div>
   )
 }
