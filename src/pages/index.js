6499041eb8224a7914aee63e9bfd0f3d379206df
SamLee514 <samuel.r.lee@vanderbilt.edu>
2020-08-26 13:49:40 -0400

    hopefully this addresses scaling

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import SEO from "../components/seo"
 import * as styles from './index.module.css'
 
 const IndexPage = () => (
+  <meta name='viewport' content='width=device, initial-scale=1.0' />
   <div id={styles.main}>
     <ScrollLock isActive={true} />
     <SEO title="Home" />
