de6182fa2e7b222714325c10ec05911c22227bb1
SamLee514 <samuel.r.lee@vanderbilt.edu>
2020-08-28 12:56:09 -0400

    ultimate hack to refresh page if orientation change

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import ScrollLock from 'react-scrolllock'
+import window from 'global'
 import { useMediaQuery } from 'react-responsive';
 
 import Dashboard from '../dashboard/dashboard';
@@ -9,9 +10,18 @@ import Game from '../game/game';
 import * as styles from './layout.module.css';
 const Layout: ((props?: any) => JSX.Element) = () => {
   const [menu, showMenu] = useState(true);
-  const isTabletOrMobileDevice = useMediaQuery({ maxDeviceWidth: 1224 })
-  const isLandscape = useMediaQuery({ orientation: 'landscape' })
-
+  const isTabletOrMobileDevice = useMediaQuery({ maxDeviceWidth: 1224 });
+  const isLandscape = useMediaQuery({ orientation: 'landscape' });
+  const deltaMobile = isTabletOrMobileDevice;
+  const deltaLandscape = isLandscape;
+  console.log('isTabletOrMobileDevice:', isTabletOrMobileDevice);
+  console.log('isLandscape:', isLandscape)
+  useEffect(() => {
+    if (isTabletOrMobileDevice != deltaMobile || deltaLandscape != isLandscape) {
+      window.location.reload(true);
+    }
+    
+  }, [isTabletOrMobileDevice, isLandscape])
 
   return (
     <>
