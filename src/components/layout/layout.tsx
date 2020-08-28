37a78f29a84a1e330b9b5d33d269225b6fee0394
SamLee514 <samuel.r.lee@vanderbilt.edu>
2020-08-28 14:38:45 -0400

    putting placeholder in container div

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -18,13 +18,15 @@ const Layout: ((props?: any) => JSX.Element) = () => {
       <ScrollLock isActive={true} />
       {/* Landscape breaks things lol */}
       { isTabletOrMobileDevice ?
-      <div id={styles.mobilePlaceholder}>
-        <div>
-          VandyHacks VII mobile edition is coming soon! For now, please visit us on desktop or
-          <a href='https://apply.vandyhacks.org/'> apply!</a>
-        </div>
-        <img src={logo} />
-      </div>      
+      <div>
+        <div id={styles.mobilePlaceholder}>
+          <div>
+            VandyHacks VII mobile edition is coming soon! For now, please visit us on desktop or
+            <a href='https://apply.vandyhacks.org/'> apply!</a>
+          </div>
+          <img src={logo} />
+        </div>   
+      </div>
       :
       <div>
         <Game showMenu={showMenu} />
