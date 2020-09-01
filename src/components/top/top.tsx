4d5b43742e196efbf00a40a11dffb56b7faf9cf8
SamLee514 <samuel.r.lee@vanderbilt.edu>
2020-09-01 13:36:28 -0400

    better display for about text

--- src/components/top/top.tsx
+++ src/components/top/top.tsx
@@ -10,20 +10,11 @@ import DownArrow from '../../assets/downarrow.png';
 
 const Top: ((props?: any) => JSX.Element) = () => {
     const topText = 'October 2-4'
-    const bottomText = 'VandyHacks is Vanderbilt’s student-run hackathon, where participants from all over the country create innovative and meaningful projects over the course of 36 hours. This year, our hackathon will be held virtually on July 10th-12th, meaning you can participate from the comfort of your own home! Come learn new topics with recorded workshops ranging from open source code to creating your own SlackBot. Meet and unwind with other fellow hackers with a round of Skribbl.io or virtual baking. Your project also has the chance to win some of our cool prizes! '
+    const bottomText = 'VandyHacks is Vanderbilt’s student-run hackathon, where participants from all over the country create innovative and meaningful projects over the course of 36 hours. This year, our hackathon will be held virtually on October 2nd-4th, meaning you can participate from the comfort of your own home! Come learn new topics with recorded workshops ranging from open source code to creating your own SlackBot. Meet and unwind with other fellow hackers with a round of Skribbl.io or virtual baking. Your project also has the chance to win some of our cool prizes! '
     const [arrowDirection, setArrowDirection] = useState('down');
     const [content, setContent] = useState(<div id={styles.dates}>{topText}</div>)
 
-    function toggleDisplay() {
-        if (arrowDirection == 'down') {
-            setArrowDirection('up');
-            setContent(<div>{bottomText}</div>);
-        }
-        else {
-            setArrowDirection('down');
-            setContent(<div id={styles.dates}>{topText}</div>)
-        }
-    }
+
 
     return (
         <div id={styles.top} className='nes-container is-dark is-rounded'>
@@ -32,10 +23,9 @@ const Top: ((props?: any) => JSX.Element) = () => {
                 <img src={Maintitle}></img>
                 <img src={Subtitle}></img>
             </div>
-            <div id={styles.about}>
-                {content}
-            </div>
-            <img id={styles[arrowDirection]} className={styles.animateBlink} onClick={toggleDisplay} src={DownArrow} alt="DownArrow" />
+            <div id={styles.dates}>{topText}</div>
+            <div id={styles.about}>{bottomText}</div>
+            {/* <img id={styles[arrowDirection]} className={styles.animateBlink} onClick={toggleDisplay} src={DownArrow} alt="DownArrow" /> */}
         </div>
     )
 }
